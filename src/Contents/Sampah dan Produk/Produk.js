25a419f4e9da4d891b979b5403fa81109cd7e2dc
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2022-12-02 08:02:52 +0700

    add product list

---
+++ src/Contents/Sampah dan Produk/Produk.js
@@ -0,0 +1,94 @@
+import { useState,useEffect } from 'react'
+import Navbar from '../../Components/Navbar'
+import { Helmet } from 'react-helmet'
+import { BsBagDashFill, BsSearch } from 'react-icons/bs'
+import { useParams } from 'react-router-dom';
+
+const jenisSampah = [{title:'All', routeRequest:''},
+                    {title:'Plastik', routeRequest:'plastik'}, 
+                    {title:'Kaca', routeRequest:'kaca'}, 
+                    {title:'Kertas', routeRequest:'kertas'}, 
+                    {title:'Kaleng', routeRequest:'kaleng'}, 
+                    {title:'Limbah Elektronik', routeRequest:'limbahelektronik'}]
+
+
+const FilterJenisSampah = () => {
+    return (
+        <div>
+            <div className=' mt-3 pl-2 pr-5 py-3 shadow-md'>
+                <h1 className='font-semibold text-xl mx-2 border-b pb-3'>Jenis Sampah - (Jenis Sampah)</h1>
+                <ul className='mx-2 space-y-2 mt-2'>
+                    {jenisSampah.map((jnsp) => (
+                        <li><a href={`/produk/${jnsp.routeRequest}`}>{jnsp.title}</a></li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+
+const SingleProduct = ({nama, harga}) => {
+  return (
+    <div className='border rounded-lg'>
+        <div className='bg-gray-500 w-full h-28 rounded-t-lg'></div>
+        <div className='space-y-2 ml-4 mt-4 mr-16'>
+            <h1 className='font-semibold text-xl'>{nama}</h1>
+            <h2 className='font-semibold text-xl'>Rp.{harga.toLocaleString('en-US')}</h2>
+            <button className='bg-green-600 py-1 rounded-md px-3 text-white'>Add to Cart</button>
+        </div>
+    </div>
+  )
+}
+
+
+
+function Produk() {
+    const jenisProduk = Object.values(useParams("plastik"))
+    const [dataProduk, setDataProduk] = useState(null)
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+        .then((response) => response.json())
+        .then((json) => setDataProduk(json))
+    }, []);
+
+    if (dataProduk === null) {
+        return (
+            <div>
+                <Navbar/>
+                <p className='text-center mt-10 text-2xl animate-ping'>Loading...</p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <Helmet>
+                <title>Title | Sampah</title>
+            </Helmet>
+            <Navbar/>
+            <form className='mx-60 mt-5 sticky top-24'>   
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <BsSearch className='text-gray-300'/>
+                    </div>
+                    <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border rounded-lg bg-gray-200 drop-shadow-xl hover:drop-shadow-md duration-150" placeholder="Cari Barang disini..." required/>
+                    <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-green-700 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+                </div>
+            </form>
+            <section className='px-60 mt-10 flex space-x-5'>
+                <aside>
+                    <h1 className='font-semibold text-xl'>Filter</h1>
+                    <FilterJenisSampah />
+                </aside>
+                <aside className='grid grid-cols-4 space-x-5 '>
+                    {dataProduk.map((ds) => (
+                        <SingleProduct nama={ds.nama_produk} harga={ds.harga} />
+                    ))}
+                </aside>
+            </section>
+        </div>
+    )
+}
+
+export default Produk
--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NotFound from './Contents/404nf';
 import JenisSampah from './Contents/Sampah dan Produk/JenisSampah';
 import Sampah from './Contents/Sampah dan Produk/Sampah';
+import Produk from './Contents/Sampah dan Produk/Produk';
 
 const Router = () => (
   <Routes>
     <Route path='/' element={<App/>}/>
     <Route path='/sampah' element={<JenisSampah/>}/>
     <Route path='/sampah/:jenis' element={<Sampah/>}/>
+    <Route path='/produk/' element={<Produk/>}/>
+    <Route path='/produk/:jenis' element={<Produk/>}/>
     <Route path='*' element={<NotFound/>}/>
   </Routes>
 )
