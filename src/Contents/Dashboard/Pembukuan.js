df6134842654d2d26d116afac2cd681570f04cc5
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2022-12-14 10:11:08 +0700

    refine table pembukuan

--- src/Contents/Dashboard/Pembukuan.js
+++ src/Contents/Dashboard/Pembukuan.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate, json } from 'react-router-dom'
 import Navbar from '../../Components/Navbar'
+import axios from 'axios';
 
 export default function Pembukuan() {
+    const [pembukuan, setPembukuan] = useState([])
+    const [jumlahPenjualan, setJumlahPenjualan] = useState()
     const params = String(Object.values(useParams()))
-  return (
+    const fetchData = async() => {
+        const response = await axios.get('http://localhost:5000/pembukuan')
+        setPembukuan(response.data)
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    
+    return (
     <div>
         <Navbar/>
         <div className='grid grid-cols-5 gap-10'>
@@ -27,7 +39,6 @@ export default function Pembukuan() {
             <div className=' col-span-4 pt-5 pb-16 pl-10'>
                 <div className='flex justify-between mr-60'>
                     <h1 className='text-2xl font-semibold'>Halaman Dashboard Pembukuan </h1>
-                    {/* <a className='bg-green-600 text-white p-3 rounded-lg hover:bg-green-500 duration-150' href={`/dashboard/${params}/create`}>Tambah { params === 'sampah' ? "Sampah" : "Produk"}</a> */}
                 </div>
                 <div className='mr-60 mt-5 space-y-3'>
                     <table className='w-full'>
@@ -35,16 +46,25 @@ export default function Pembukuan() {
                             <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>No.</th>
                             <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Nama Member</th>
                             <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Tanggal Penjualan</th>
-                            <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Jenis Barang</th>
+                            <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Ketegori Penjualan</th>
+                            <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Jenis Sampah</th>
                             <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Total Penjualan</th>
+                            <th className='border border-slate-300 px-3 py-2 text-left bg-slate-600 text-white'>Action</th>
                         </tr>
-                        <tr>
-                            <td className='border border-slate-300 px-3 bg-slate-100'>1</td>
-                            <td className='border border-slate-300 px-3 bg-slate-100'>Ali</td>
-                            <td className='border border-slate-300 px-3 bg-slate-100'>1-Jan-2000</td>
-                            <td className='border border-slate-300 px-3 bg-slate-100'>Plastik</td>
-                            <td className='border border-slate-300 px-3 bg-slate-100'>107</td>
-                        </tr>
+                        {pembukuan.map((pmb, i) => (
+                            <tr>
+                                <td className='border border-slate-300 px-3 bg-slate-100'>{i+1}</td>
+                                <td className='border border-slate-300 px-3 bg-slate-100'>{pmb.nama_member}</td>
+                                <td className='border border-slate-300 px-3 bg-slate-100'>{pmb.tanggal_penjualan}</td>
+                                <td className='border border-slate-300 px-3 bg-slate-100 capitalize'>{pmb.jenis_sampah}</td>
+                                <td className='border border-slate-300 px-3 bg-slate-100 capitalize'>{pmb.kategori_penjualan}</td>
+                                <td className='border border-slate-300 px-3 bg-slate-100'>{pmb.total_penjualan}</td>
+                                <td className='border border-slate-300 px-3 bg-slate-100 space-x-5'>
+                                    <a className='text-green-700'>Edit</a>
+                                    <a className='text-red-700'>Delete</a>
+                                </td>
+                            </tr>
+                        ))}
                     </table>
                 </div>
             </div>
