ec15490fc49a236f0aadb6bef02590a0ec968730
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2022-12-15 11:37:50 +0700

    refine form login/regist

--- src/Contents/Login Register/Register.js
+++ src/Contents/Login Register/Register.js
@@ -6,7 +6,7 @@ function Register() {
     <div className='bg-gray-200 h-screen'>
         <Navbar/>
         <div className='px-60 mt-28'>
-            <div className='border mx-[500px] p-5 bg-white drop-shadow-lg'>
+            <form className='border mx-[500px] p-5 bg-white drop-shadow-lg'>
                 <h1 className='text-center font-semibold text-3xl'>Register</h1>
                 <hr className='mt-5'/>
                 <div className='mt-5'>
@@ -28,7 +28,7 @@ function Register() {
                 <div className='mt-7'>
                     <a className='bg-green-600 text-white px-3 py-2'>Register</a>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
   )
