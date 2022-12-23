3eefd847e516be124d66c6ec4489774f43326577
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2022-12-23 08:18:46 +0700

    fix: pembukuan

--- src/Contents/Dashboard/Pembukuan.js
+++ src/Contents/Dashboard/Pembukuan.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, useNavigate, json } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
 import axios from 'axios';
 import AuthNavbar from '../../Components/AuthNavbar';
@@ -9,7 +9,6 @@ import AuthInformation from './AuthInformation'
 
 export default function Pembukuan() {
     const [pembukuan, setPembukuan] = useState([])
-    const [jumlahPenjualan, setJumlahPenjualan] = useState()
     const [querySearch, setQuerySearch] = useState("")
     const params = String(Object.values(useParams()))
     const dispatch = useDispatch();
@@ -36,7 +35,7 @@ export default function Pembukuan() {
     }
     useEffect(()=>{
         fetchData()
-    },[querySearch])
+    },[querySearch, fetchData])
 
     const delData = async (Id) => {
         try {
